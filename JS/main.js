1066e0c7500d169fb614f115f5bcc7f21289ee5e
MarcoAlarcon <marcoh.alarcon@icloud.com>
2022-12-05 00:30:25 -0300

    commitando o projeto no repositorio

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+let livros = []
+const endpointAPI = "https://guilhermeonrails.github.io/casadocodigo/livros.json"
+getBuscarLivrosDaAPI()
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch (endpointAPI)
+    livros = await res.json()
+    let livrosComDesconto = aplicarDesconto(livros)
+    botarLivros(livrosComDesconto)
+}
